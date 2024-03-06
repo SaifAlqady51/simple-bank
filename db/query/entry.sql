3c206dc7b246d2ca45872efd6847dd43ec7fa202
SaifAlqady51 <saifalqady51@gmail.com>
2024-03-06 16:41:46 +0200

    feat: created entries CRUD operation

---
+++ db/query/entry.sql
@@ -0,0 +1,30 @@
+
+-- name: CreateEntry :one
+INSERT INTO enteries (
+    account_id,
+    amount 
+) VALUES (
+    $1, $2
+)
+ RETURNING * ;
+
+-- name: GetEntry :one
+SELECT * FROM enteries
+WHERE id = $1 LIMIT 1 ;
+
+-- name: ListEntry :many
+SELECT * FROM enteries
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateEntry :one
+UPDATE enteries
+SET amount = $2
+WHERE $1
+RETURNING *;
+
+-- name: DeleteEntry :one
+DELETE FROM enteries
+WHERE id = $1
+RETURNING *;
