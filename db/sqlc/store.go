e42b3eff450a9c77bd93e4d06cfbaf49607e2970
SaifAlqady51 <saifalqady51@gmail.com>
2024-03-10 19:36:28 +0200

    db: Added update balance functionallity in TransferTx function

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -20,19 +20,29 @@ func NewStore(db *sql.DB) *Store {
 
 // execTX execute a function within database transaction
 func (store *Store) execTX(ctx context.Context, fn func(*Queries) error) error {
+
+	// start a new transaction
 	tx, err := store.db.BeginTx(ctx, nil)
+
+	// return err if exists
 	if err != nil {
 		return err
 	}
+	// create new Query from tx db
 	q := New(tx)
+
+	// execute fn function that takes query and returns error if exists
 	err = fn(q)
+
 	if err != nil {
+		// if any error occurs during the transaction rollback the changes
 		if rbErr := tx.Rollback(); rbErr != nil {
+			// print out the error
 			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
 		return err
 	}
-
+	// if transaction succedded Commit the transation
 	return tx.Commit()
 }
 
@@ -53,18 +63,23 @@ type TransferTxResult struct {
 //TransferTx performs a money transfer from one account another
 
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	// initiat result variable
 	var result TransferTxResult
 
+	// check if store.execTX would returns an error
 	err := store.execTX(ctx, func(q *Queries) error {
 		var err error
+		// get result.Transfer from Create Transfer option or get the error
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountId,
 			ToAccountID:   arg.ToAccountId,
 			Amount:        arg.Amount,
 		})
+		// return err if exists
 		if err != nil {
 			return err
 		}
+		// get FromEntry prop from CreateEntry function and pass its valut to  result.FromEntry *TransferTxResult
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.FromAccountId,
 			Amount:    -arg.Amount,
@@ -72,6 +87,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		if err != nil {
 			return err
 		}
+		// get ToEntry prop from CreateEntry function and pass its valut to  result.ToEntry *TransferTxResult
 		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.ToAccountId,
 			Amount:    arg.Amount,
@@ -79,8 +95,23 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		if err != nil {
 			return err
 		}
+		// update from account balance
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountId,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
-		// TODO: update accounts' balance
+		// get to account query
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountId,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 	return result, err
