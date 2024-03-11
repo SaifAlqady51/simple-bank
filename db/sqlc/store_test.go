f7dc5856838091837e1feb6ec83671a4939710ca
SaifAlqady51 <saifalqady51@gmail.com>
2024-03-11 23:02:02 +0200

    test: Tested deadlock solution

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTransfer(t *testing.T) {
+func TestTransferTx(t *testing.T) {
 	store := NewStore(testDB)
 
 	account1 := createRandomAccount(t)
@@ -109,5 +109,55 @@ func TestTransfer(t *testing.T) {
 	updateAccount2, err := testQuery.GetAccount(context.Background(), account2.ID)
 	require.NoError(t, err)
 	require.Equal(t, account2.Balance+int64(n)*amount, updateAccount2.Balance)
+
+}
+
+func TestTransferTxDeadloack(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	// run n concurrent transfer transaction
+
+	n := 10
+
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountId := account1.ID
+		toAccountId := account2.ID
+
+		if i%2 == 1 {
+			fromAccountId = account2.ID
+			toAccountId = account1.ID
+
+		}
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountId: fromAccountId,
+				ToAccountId:   toAccountId,
+				Amount:        amount,
+			})
+			errs <- err
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+	}
+	// check the final updated balance
+	updateAccount1, err := testQuery.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.Equal(t, account1.Balance, updateAccount1.Balance)
+
+	updateAccount2, err := testQuery.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+	require.Equal(t, account2.Balance, updateAccount2.Balance)
 
 }
