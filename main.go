7963835eedacb07b38285c4d872d0ce89d5e162a
SaifAlqady51 <saifalqady51@gmail.com>
2024-03-17 17:00:46 +0200

    feat: Added config file with vipar

--- main.go
+++ main.go
@@ -6,24 +6,23 @@ import (
 
 	"github.com/SaifAlqady51/simple-bank/api"
 	db "github.com/SaifAlqady51/simple-bank/db/sqlc"
+	"github.com/SaifAlqady51/simple-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple-bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("we can't connect to the database")
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start a server ", err)
 	}
