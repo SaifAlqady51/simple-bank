64c393b019f5303128679e2fceb6fd664a944097
SaifAlqady51 <saifalqady51@gmail.com>
2024-04-06 21:49:28 +0200

    feat: Created userlogin api functionallity

--- main.go
+++ main.go
@@ -24,7 +24,10 @@ func main() {
 	// new Store
 	store := db.NewStore(conn)
 	// start a new Server
-	server := api.NewServer(store)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("Cannot start a server ", err)
+	}
 
 	err = server.Start(config.ServerAddress)
 	if err != nil {
